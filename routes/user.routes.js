770d2d9e67b10aa69d2a087dbc7daeb19841e5fe
priyanshk <kapadia.priyansh@gmail.com>
2025-05-30 02:25:33 +0530

    added login page/JWT authenticationn

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const { body,validationResult } = require('express-validator');
 const router = express.Router();
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/register', (req,res) => {
     res.render('register');
@@ -33,6 +34,56 @@ router.post('/register', body('username').notEmpty().withMessage('Name is requir
 
     console.log(req.body);
     res.json(newUser);
+})
+
+router.get('/login', (req,res) => {
+    res.render('login');
+})
+
+router.post('/login',  body('username').notEmpty().withMessage('Name is required'),
+                        body('password').isLength({min: 8}).withMessage('Password must be at least 8 characters long'),
+    async(req, res) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "invalid input"
+            })
+        }
+
+        const {username, password} = req.body;
+ 
+        const user = await userModel.findOne({
+            username: username
+        })
+
+        if(!user) {
+            return res.status(400).json({
+                message: "User or password is incorrect"
+                //use this message always to avoid giving hints to attackers
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({
+                message:"User or password is incorrect"
+                //use this message always to avoid giving hints to attackers
+            })
+        }
+
+        //generate a token here if you want to implement JWT authentication
+        const token = jwt.sign({
+            userId: user._id,
+            username: user.username,
+            email: user.email
+        }, process.env.JWT)
+        
+        res.cookie('token', token);
+
+        res.send('Login successful');
 })
 
 module.exports = router;
